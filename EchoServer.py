5cc767e73f7421d30742e089ec7e9bdcc6002b8c
Unknown <mazznielsen@hotmail.com>
2018-02-08 11:51:21 +0100

    added everything

--- EchoServer.py
+++ EchoServer.py
@@ -1,34 +1,42 @@
 import socket
-import sys
+import time
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with open("ServerLog.txt", "a") as f:
 
-# Bind the socket to the port
-server_address = ('localhost', 6789)
-print ( 'starting up on %s port %d' % server_address)
-sock.bind(server_address)
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Listen for incoming connections
-sock.listen(1)
+    # Bind the socket to the port
+    server_address = ('localhost', 6789)
+    print ( 'starting up on %s port %d' % server_address)
+    f.write('starting up on %s port %d' % server_address)
+    sock.bind(server_address)
 
-while True:
-    #wait for connection
-    print ( 'waiting for connection')
-    connection, client_address = sock.accept()
+    #Listen for incoming connections
+    sock.listen(1)
 
-    try:
-        print ( 'connection from', client_address)
+    while True:
+        #wait for connection
+        print ( 'waiting for connection...')
+        f.write('waiting for connection...')
+        connection, client_address = sock.accept()
 
-        #receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(1024)
-            print ('received "%s" ' % data)
-            if data:
-                print ( 'sending data back to the client')
-            else:
-                print('no more data from', client_address)
-                break
-    finally:
-        #Clean up the connection
-        connection.close()
+        try:
+            print ( 'connection from', client_address)
+            f.write('connection from', client_address)
+
+            #receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(1024)
+                print ('received "%s" ' % data)
+                f.write('received "%s" ' % data)
+                if data:
+                    print ( 'sending data back to the client')
+                    f.write('sending data back to the client')
+                else:
+                    print('no more data from', client_address)
+                    f.write('no more data from', client_address)
+                    break
+        finally:
+            #Clean up the connection
+            connection.close()
