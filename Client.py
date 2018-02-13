42de9451ea0cceb3bca81faf2c4a6c48f41d1435
Unknown <alla2634@edu.easj.dk>
2018-02-13 13:20:17 +0100

    changes

--- Client.py
+++ Client.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
 
+import getopt
 import socket
+import sys
 
-HOST = '127.0.0.1'
-PORT = 6789
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 's:p:f:h', ['src=', 'port=', 'file=', 'help'])
+except getopt.GetoptError as e:
+    print(e)
+    exit(2)
+
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        exit(2)
+    elif opt in ('-d', '--destination'):
+        HOST = arg
+    elif opt in ('-p', '--port'):
+        PORT = arg
+    elif opt in ('-f', '--file'):
+        FILE = arg
+    else:
+        exit(2)
+
 REQUEST = "GET / HTTP/1.1\r\nHost:{}:{}\r\n".format(HOST, PORT)
 
 print("[*] Connecting to {}:{}".format(HOST, PORT))
@@ -21,3 +40,4 @@ msg = sock.recv(1024)
 
 sock.close()
 print(msg.decode('utf-8'))
+
