e0f87394fdb5b589bf1637ed0b3083dd1076767d
Unknown <alla2634@edu.easj.dk>
2018-02-13 13:29:21 +0100

    Client args works!

--- Client.py
+++ Client.py
@@ -6,7 +6,7 @@ import sys
 
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 's:p:f:h', ['src=', 'port=', 'file=', 'help'])
+    opts, args = getopt.getopt(sys.argv[1:], 'd:p:f:h', ['dest=', 'port=', 'file=', 'help'])
 except getopt.GetoptError as e:
     print(e)
     exit(2)
@@ -14,22 +14,22 @@ except getopt.GetoptError as e:
 for opt, arg in opts:
     if opt in ('-h', '--help'):
         exit(2)
-    elif opt in ('-d', '--destination'):
+    elif opt in ('-d', '--dest'):
         HOST = arg
     elif opt in ('-p', '--port'):
-        PORT = arg
+        PORT = int(arg)
     elif opt in ('-f', '--file'):
         FILE = arg
     else:
         exit(2)
 
-REQUEST = "GET / HTTP/1.1\r\nHost:{}:{}\r\n".format(HOST, PORT)
+REQUEST = "GET /{} HTTP/1.1\r\nHost:{}:{}\r\n".format(FILE, HOST, PORT)
 
 print("[*] Connecting to {}:{}".format(HOST, PORT))
-# create a socket object
+# Create a socket object
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connection to hosocktname on the port.
+# Connection to hosocktname on the port.
 sock.connect((HOST, PORT))
 print("[*] Connected to {}:{}".format(HOST, PORT))
 
